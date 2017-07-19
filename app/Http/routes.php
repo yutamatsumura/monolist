33454454220d0bdc3968141e08c025ecd82e71d1
yutamatsumura <yuta.matsumura@bravi.co.jp>
2017-07-19 08:21:05 +0000

    rakuten api

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,3 +21,7 @@ Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
 Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
 Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
 Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('items', 'ItemsController', ['only' => ['create']]);
+});
