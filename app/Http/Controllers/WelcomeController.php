da68a2118d4f749b42afd76e98f3ede6bf0abb5b
yutamatsumura <yuta.matsumura@bravi.co.jp>
2017-07-19 01:53:35 +0000

    top page

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Item;
+
+class WelcomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
+}
